b112f274b2594f4cf5042bc0ef174124d28a7704
Chris Spencer <chris.ag.spencer@googlemail.com>
2014-12-03 12:28:04 +0000

    Consistent compile clause layout

--- lib/message_format.ex
+++ lib/message_format.ex
@@ -156,13 +156,16 @@ defmodule MessageFormat do
 
   def compile({:select, arg, m}, env) do
     arg = arg |> String.downcase |> String.to_atom
+    accessor = quote do
+      unquote(var(:args))[unquote(arg)]
+    end
 
     clauses = Enum.map(m, fn({k, v}) ->
       {:->, [], [[k], compile(v, env)]}
     end)
 
     quote do
-      case unquote(var(:args))[unquote(arg)] do
+      case unquote(accessor) do
         unquote(clauses)
       end
     end
