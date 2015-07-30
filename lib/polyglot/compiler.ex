e0b56ecfe505a46f6dbd61b6d9c7237dadaf6c0e
Chris Spencer <chris.ag.spencer@googlemail.com>
2015-07-30 21:04:47 +0100

    Split Lang file loading to separate file

--- lib/polyglot/compiler.ex
+++ lib/polyglot/compiler.ex
@@ -12,29 +12,6 @@ defmodule Polyglot.Compiler do
     {[lang, key, args], ast}
   end
 
-  # Load a file into [{name, string}, ...]
-  def load_file(path) do
-    {messages, name, buffer} = File.stream!(path)
-                               |> Enum.reduce({[], nil, nil}, &parse_line(&1, &2))
-
-    [{name, String.strip(buffer)}|messages]
-  end
-
-  defp parse_line(line, state) do
-    case {line, state} do
-      {<<"@", newname::binary>>, {messages, nil, nil}} ->
-        {messages, String.strip(newname), ""}
-      {<<"@", newname::binary>>, {messages, name, buffer}} ->
-        {[{name, String.strip(buffer)}|messages], String.strip(newname), ""}
-      {<<"--", _::binary>>, state} ->
-        state
-      {_, {_, nil, nil}=state} ->
-        state
-      {line, {messages, name, buffer}} ->
-        {messages, name, "#{buffer}\n#{line}"}
-    end
-  end
-
   # Parse a string to an ast
   def parse(str) do
     {:ok, tokens} = tokenise(str, {"", [], 0})
---
+++ lib/polyglot/lang.ex
@@ -0,0 +1,25 @@
+defmodule Polyglot.Lang do
+
+  # Load a file into [{name, string}, ...]
+  def load_file(path) do
+    {messages, name, buffer} = File.stream!(path)
+                               |> Enum.reduce({[], nil, nil}, &parse_line(&1, &2))
+
+    [{name, String.strip(buffer)}|messages]
+  end
+
+  defp parse_line(line, state) do
+    case {line, state} do
+      {<<"@", newname::binary>>, {messages, nil, nil}} ->
+        {messages, String.strip(newname), ""}
+      {<<"@", newname::binary>>, {messages, name, buffer}} ->
+        {[{name, String.strip(buffer)}|messages], String.strip(newname), ""}
+      {<<"--", _::binary>>, state} ->
+        state
+      {_, {_, nil, nil}=state} ->
+        state
+      {line, {messages, name, buffer}} ->
+        {messages, name, "#{buffer}\n#{line}"}
+    end
+  end
+end
